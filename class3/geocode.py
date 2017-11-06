b875ab5ac47162a93a114d067a0ae7754fe5f52d
fitnr <fitnr@fakeisthenewreal>
2017-11-06 18:18:36 -0500

    add in-class files

--- class3/geocode.py
+++ class3/geocode.py
@@ -1,3 +1,6 @@
+import csv
+from censusgeocode import CensusGeocode
+import time
 
 counties = {
     'BX': 'Bronx',
@@ -6,3 +9,35 @@ counties = {
     'K': 'Brooklyn',
     'R': 'Staten Island'
 }
+
+cg = CensusGeocode()
+
+with open('citations.csv') as f:
+    reader = csv.DictReader(f)
+
+    with open('new-citations.csv', 'a') as g:
+        newfieldnames = reader.fieldnames + ["address", "latitude", "longitude"]
+        writer = csv.DictWriter(g, fieldnames=newfieldnames)
+        writer.writeheader()
+
+        for row in reader:
+            house = row['house_number']
+            street = row['street_name']
+            county = row['violation_county']
+
+            address = '{} {}, {} NY'.format(house, street, counties.get(county, 'New York City'))
+
+            try:
+                locations = cg.onelineaddress(address, returntype='locations')
+                lng = locations[0]['coordinates']['x']
+                lat = locations[0]['coordinates']['y']
+            except:
+                lng = None
+                lat = None
+
+            row['address'] = address
+            row["latitude"] = lat
+            row["longitude"] = lng
+
+            writer.writerow(row)
+            time.sleep(0.001)
